cc43f517f2b967e25d6ebaafb6d8ce4ac9c2563e
Fabian Neumann <Fabian Neumann@DESKTOP-VD6NPMC>
2017-06-20 10:02:48 +0100

    merge parameters and runner

--- Parameters.py
+++
@@ -1,4 +0,0 @@
-import configparser
-config = configparser.ConfigParser()
-config.read('parameters/evalParams.ini')
-print (config.getboolean('uncertainty', 'unc_ev',fallback='Request not in parameter set!'))
