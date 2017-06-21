1fdbe7a3dcee55d2c5d575300584dddb3cbd06ad
Fabian Neumann <Fabian Neumann@DESKTOP-VD6NPMC>
2017-06-21 19:36:30 +0100

    household specifications

---
+++ src/HouseholdSpecifications.py
@@ -0,0 +1,26 @@
+import numpy.random as rd
+
+class Household:
+
+    def __init__(self):
+        x = rd.random()
+        self.inhabitants = 0
+        self.id_range = 0
+        if x < 0.34:
+            self.inhabitants = 1
+            self.id_range = 340
+        elif x < 0.74:
+            self.inhabitants = 2
+            self.id_range = 400
+        elif x < 0.88:
+            self.inhabitants = 3
+            self.id_range = 140
+        elif x < 0.97:
+            self.inhabitants = 4
+            self.id_range = 90
+        else:
+            self.inhabitants = 5
+            self.id_range = 30
+        
+        self.demandForecast = []
+        self.demandSimulation = []
--- src/SimulationFramework.py
+++ src/SimulationFramework.py
@@ -9,16 +9,20 @@ import csv
 import fileinput
 from statistics import mean
 import measurement.measures as conv
+from operator import add
+
 # class imports
 from VehicleSpecifications import ElectricVehicle
+from HouseholdSpecifications import Household
 
 # ****************************************************
 # * General Framework Initialisation
 # ****************************************************
+
 # Administrative
 rd.seed(1932455)
 
-# some utility functions
+# Utility functions
 def read_timeseries(filename):
     with open(filename) as file:
         data = [float(line) for line in file]
@@ -32,9 +36,16 @@ def merge_timeseries(x,y):
         else:
             z.append(y[i-dayswitch_slot])
     return z
+
+def updateLoad(ts,id):
+    dmd_dss = str(ts).replace(',', '').replace('[', '').replace(']', '')
+    DSSText.Command = "Edit Loadshape.Shape_"+str(id)+" mult=("+dmd_dss+")"
+    DSSText.Command = "Edit Load.LOAD"+str(id)+" daily=Shape_"+str(id)
+    
 # ****************************************************
 # * Read Parameters
 # ****************************************************
+
 cfg = configparser.ConfigParser()
 cfg.read("../parameters/evalParams.ini")
 
@@ -42,7 +53,6 @@ cfg.read("../parameters/evalParams.ini")
 par_surcharge = cfg.getfloat("market_prices","surcharge")
 par_spread = cfg.getfloat("market_prices","spread")
 par_evpenetration = cfg.getfloat("electric_vehicles","penetration")
-
 start = conv.Time(hr=cfg.getfloat("general","starting")).min 
 duration = conv.Time(hr=cfg.getint("general","duration")).min
 resolution = cfg.getint("general","resolution")
@@ -61,101 +71,95 @@ num_households = 55
 # ****************************************************
 
 # Instantiate the OpenDSS Object
-print(">> Opening DSS Engine.")
 try:
     DSSObj = win32com.client.Dispatch("OpenDSSEngine.DSS")
 except:
-    print("Unable to start the OpenDSS Engine")
+    print("<< @Init: Unable to start the OpenDSS Engine")
     raise SystemExit
+
+print(">> @Init: OpenDSS engine opened.")
 
 # Set up the Text, Circuit, and Solution Interfaces to manage OpenDSS
 DSSText = DSSObj.Text
 DSSCircuit = DSSObj.ActiveCircuit
 DSSSolution = DSSCircuit.Solution
 
-print(">> Compiling distribution network.")
+# Compile and set major parameters
 DSSText.Command = r"Compile '..\network_details\Master.dss'"
 DSSText.Command = "set mode=daily number="+str(num_slots)+" stepsize="+str(resolution)+"m"
 
-print(">> OpenDSS network instantiated and compiled.")
+for i in range(num_households):
+    DSSText.Command = "New Loadshape.Shape_"+str(i+1)
+    DSSText.Command = "~ npts="+str(num_slots)
+    DSSText.Command = "~ minterval="+str(resolution)
+    DSSText.Command = "~ useactual=true"
+    
+print(">> @Init: Network instantiated and compiled.")
+
 # ****************************************************
 # * Generate Scenario
 # ****************************************************
+print("-------------------------------------------------")
 
 # assign residential load forecast in network
-
-### number of inhabitants according to CREST model
-x = rd.random()
-num_inhabitants = 0
-if x < 0.34:
-    num_inhabitants = 1
-    id_range = 340
-elif x < 0.74:
-    num_inhabitants = 2
-    id_range = 400
-elif x < 0.88:
-    num_inhabitants = 3
-    id_range = 140
-elif x < 0.97:
-    num_inhabitants = 4
-    id_range = 90
-else:
-    num_inhabitants = 5
-    id_range = 30
-
-day_id = rd.randint(1,id_range)
-demand_ts1 = read_timeseries("../demand_timeseries/loadprofile_"+season+"_inh"+\
-                             str(num_inhabitants)+"_"+str(resolution)+"min"+format(day_id,"03d")+".txt")
-day_id = rd.randint(1,id_range)
-demand_ts2 = read_timeseries("../demand_timeseries/loadprofile_"+season+"_inh"+\
-                             str(num_inhabitants)+"_"+str(resolution)+"min"+format(day_id,"03d")+".txt")
-demand_ts = merge_timeseries(demand_ts1,demand_ts2)
-
-dmd_dss = str(demand_ts).replace(',', '').replace('[', '').replace(']', '')
-DSSText.Command = "New Loadshape.Shape_1 npts="+str(num_slots)+" minterval="+str(resolution)+" mult=("+dmd_dss+") useactual=true"
-DSSText.Command = "Plot Loadshape object=Shape_1"
-
+households = [Household() for i in range(1,num_households)]
+for hd in households:
+    counter = 1
+    day_id = rd.randint(1,hd.id_range)
+    demand_ts1 = read_timeseries("../demand_timeseries/loadprofile_"+season+"_inh"+\
+                                 str(hd.inhabitants)+"_"+str(resolution)+"min"+format(day_id,"03d")+".txt")
+    day_id = rd.randint(1,hd.id_range)
+    demand_ts2 = read_timeseries("../demand_timeseries/loadprofile_"+season+"_inh"+\
+                                 str(hd.inhabitants)+"_"+str(resolution)+"min"+format(day_id,"03d")+".txt")
+    hd.demandForecast = merge_timeseries(demand_ts1,demand_ts2)
+    updateLoad(hd.demandForecast,counter)
+    counter+=1
+print(">> @Scen: "+str(num_households)+" households initialised and demand forecasts generated.")
 
 # assign PV generation forecast in network
-# later
+# LATER
 
 # assign EV behaviour in network
 num_evs = round(par_evpenetration * num_households)
 evs = [ElectricVehicle(cfg,sps.randint(1,num_households)) for i in range(1,num_evs)]
-print(">> "+str(num_evs)+"/" +str(num_households)+" possible vehicles initialised and located.")
+print(">> @Scen: "+str(num_evs)+"/" +str(num_households)+" possible vehicles initialised and located.")
 
 # generate EV availability and battery state of charge forecast
 for ev in evs:
     ev.generateAvailabilityForecast()
     ev.generateBatterySOCForecast()
-print(">> Vehicle availability and battery SOC forecasts generated.")
+    
+print(">> @Scen: Vehicle availability and battery SOC forecasts generated.")
 
 # generate electricity price forecast
 day_id = rd.randint(0,999)
-
-price_ts1 = read_timeseries("../price_timeseries/15min/priceprofile_ukpx_15min"+format(day_id,"04d")+".txt")
-price_ts2 = read_timeseries("../price_timeseries/15min/priceprofile_ukpx_15min"+format(day_id+1,"04d")+".txt")
+price_ts1 = read_timeseries("../price_timeseries/15min/priceprofile_ukpx_"+str(resolution)+"min"+format(day_id,"04d")+".txt")
+price_ts2 = read_timeseries("../price_timeseries/15min/priceprofile_ukpx_"+str(resolution)+"min"+format(day_id+1,"04d")+".txt")
 price_ts = merge_timeseries(price_ts1, price_ts2)
-    
 mean_price = mean(price_ts)
 price_ts = [((item - mean_price) * par_spread + mean_price + par_surcharge) for item in price_ts]
-print(">> Electricity market prices forecast generated.")
+print(">> @Scen: Electricity market prices forecast generated.")
 
 # ****************************************************
 # * Run Optimisation
 # ****************************************************
+print("-------------------------------------------------")
 
 
+
+print(">> @Opt: ")
 
 # ****************************************************
 # * Run Simulation
 # ****************************************************
+print("-------------------------------------------------")
+
+print(">> @Sim: ")
 
 # Compile and solve circuit
-
 DSSSolution.Solve()
 if DSSSolution.Converged:
-    print (">> Simulation: The Circuit Solved Successfully")
+    print (">> @Sim: The Circuit Solved Successfully")
 
 #final DSS command: close demand interval files at end of run
 #DSSText.Command = "closedi" 
@@ -163,3 +167,5 @@ if DSSSolution.Converged:
 # ****************************************************
 # * Evaluation
 # ****************************************************
+print("-------------------------------------------------")
+print(">> @Eval: ")
